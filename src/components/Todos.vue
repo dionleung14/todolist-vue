6296f7fbf95f811af328454ea2eca717d4d54cd7
NA Flipper <dioncleung@gmail.com>
2020-11-08 22:28:09 -0800

    modified todo items component

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -2,13 +2,19 @@
   <div>
     <div v-for="pokemon in listTodos" v-bind:key="pokemon.id">
       <h3>{{ pokemon.title }}</h3>
+    <TodoItem />
     </div>
   </div>
 </template>
 
 <script>
-  export default {
+  import TodoItem from './TodoItem.vue'
+
+export default {
     name: "Todos",
+    components: {
+      TodoItem
+    },
     props: ["listTodos"]
   };
 </script>
