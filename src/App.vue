e2197b80d80479f92991349259eb1a150f483da9
NA Flipper <dioncleung@gmail.com>
2020-11-19 01:44:15 -0800

    added routing

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,44 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="aWholeNew" />
-    <Todos v-bind:geoduck="todosArr" v-on:rec-one-todo="removeTodo" />
-    {{ msg }}
+    <div id="nav">
+      <router-link to="/">Todos</router-link> |
+      <router-link to="/about">About</router-link> | 
+      <router-link to="/home">Home</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-  import Todos from "./components/Todos.vue";
-  import AddTodo from "./components/AddTodo.vue";
-  import Header from "./components/layout/Header.vue";
-  export default {
-    name: "App",
-    components: {
-      Todos: Todos,
-      Header,
-      AddTodo,
-    },
-    data() {
-      return {
-        msg: "Hello",
-        todosArr: [
-          // {
-          //   id: 1,
-          //   title: "todo uno",
-          //   completed: false,
-          // },
-          // {
-          //   id: 2,
-          //   title: "todo two",
-          //   completed: false,
-          // },
-          // {
-          //   id: 3,
-          //   title: "todo three",
-          //   completed: true,
-          // },
-        ],
-      };
-    },
-    created() {
-      // console.log("yeeeeeet");
-      fetch("https://jsonplaceholder.typicode.com/users/1/todos?_limit=5")
-          .then(response => 
-            // console.log(response.json());
-            // console.log("hello");
-            response.json()
-            // console.log(data);
-          )
-          .then(json => {
-            json.forEach((element) => {
-              // console.log("oi oi oi");
-              // console.log(element);
-              // this.percocet();
-              this.todosArr = [...this.todosArr, element]
-              this.todosArr.push(element)
-            });
-            // this.todosArr = json
-          })
-          .catch(err => console.log(err));
-    },
-    methods: {
-      removeTodo(id) {
-        // console.log("received in app.vue");
-        // console.log(id);
-        this.todosArr = this.todosArr.filter(item => item.id !== id);
-      },
-      aWholeNew(data) {
-        this.todosArr = [...this.todosArr, data];
-        // this.todosArr.push(data)
-        // console.log("a new todo was received")
-        // console.log(data)
-      },
-      percocet() {
-        console.log("percocet")
-      }
-    },
-  };
+import Header from "./components/layout/Header";
+export default {
+  name: "app",
+  components: {
+    Header
+  }
+}
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+#nav {
+  padding: 30px;
+}
 
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
 
-  .btn:hover {
-    background: #666;
-  }
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script>
-import { v4 as uuidv4 } from 'uuid'
+// import { v4 as uuidv4 } from 'uuid'
 export default {
   name: "AddTodo",
   data() {
@@ -20,7 +20,7 @@ export default {
     wristslap(event) {
       event.preventDefault()
       const newTodo = {
-        id: uuidv4(),
+        // id: uuidv4(),
         title: this.autumn,
         completed: false
       }
